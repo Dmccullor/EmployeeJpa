5e271921ed09b88183f4aa1a00c9a9e4e9a73f7e
Dylan McCullor <dmccullor0@gmail.com>
2022-06-21 18:07:26 -0500

    set up employee class

--- src/main/java/com/cognixia/jump/model/Employee.java
+++ src/main/java/com/cognixia/jump/model/Employee.java
@@ -1,5 +1,85 @@
 package com.cognixia.jump.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String firstName;
+	
+	@Column
+	private String lastName;
+	
+	@Column
+	private String department;
+	
+	@Column
+	private String title;
+
+	public Employee(Integer id, String firstName, String lastName, String department, String title) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.department = department;
+		this.title = title;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", department="
+				+ department + ", title=" + title + "]";
+	}
+	
+	
 
 }
