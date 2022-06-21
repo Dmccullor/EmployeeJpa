21213a0259b9372bc375756323187d5cfa256ab9
Dylan McCullor <dmccullor0@gmail.com>
2022-06-21 18:14:52 -0500

    set up jpa interface and ran to create table

--- src/main/java/com/cognixia/jump/repository/EmployeeRepository.java
+++ src/main/java/com/cognixia/jump/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.cognixia.jump.repository;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognixia.jump.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
